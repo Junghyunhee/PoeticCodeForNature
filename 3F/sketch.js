0fd2fa01c7bfb90c5fe21b42751e7db81354a49f
Junghyunhee <junghyunh22@naver.com>
2019-06-24 21:54:31 +0900

    Update sketch.js

--- 3F/sketch.js
+++ 3F/sketch.js
@@ -5,39 +5,37 @@ let ball = [];
 
 
 function setup() {
-  createCanvas(400, 400);
-  colorMode(HSB, 360, 100, [255]);
+  createCanvas(500, 500);
   gravity = createVector(0, 0.1);
   stroke(255)
   strokeWeight(1);
   for (let i = 0; i < 30; i++) {
     ball[i] = new Particle();
-    
+
   }
 }
 
 
 function draw() {
-  background(100, 0, 0, 25);
+  background(200, 0, 0);
   for (var i = 0; i < 30; i++) {
     ball[i].show();
     ball[i].update();
     ball[i].isDead();
     if (mouseIsPressed) {
       ball[i].hold();
-        
+
     }
   }
 
 }
 function mouseReleased() {
   for (let i = 0; i < 30; i++) {
-    gravity = createVector(random(0.5, -0.5), random(-0.5, 0.5));
+    gravity = createVector(random(0.5, -0.5), random(-3, 3));
     ball[i].addForce(gravity);
     wind = createVector(random(-3, 3));
     ball[i].addForce(wind);
-    
+
   }
  return false;
 }
- 
